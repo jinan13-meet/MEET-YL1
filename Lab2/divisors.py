479a9fef674e8c64ac10ccb792b8a27d1f89b64b
Jinan Zughayar <jinan13@meet.mit.edu>
2013-10-21 18:53:01 +0200

    ex-a 2 clear

--- Lab2/divisors.py
+++ Lab2/divisors.py
@@ -1,6 +1,9 @@
 
-n = raw_input("enter a number")
-for n in xrange(n):
-	print xrange + 1
+a = raw_input("enter a number")
+n= int(a)
+for num in xrange (n):
+	if n % num == 0 : 
+		print num + 1
+
 
 
